61fb5d6ee282c48ee6a22edee358a923055dd45d
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-03-11 13:20:56 -0500

    Update Backend

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -10,7 +10,7 @@ router.post("/employee", [
     check("phone").notEmpty().withMessage("Ingresa el número telefónico."),
     check("email").isEmail().withMessage("Ingresa el correo electrónico."),
     check("password").notEmpty().withMessage("Ingresa la contraseña."),
-    check("state").notEmpty().withMessage("Ingresa el estado de la cuenta."),
+    check("state").isBoolean().withMessage("Ingresa el estado de la cuenta."),
 ] , UserController.userRegisterEmployee)
 
 router.delete("/employee/:id", UserController.userDeleteEmployee);
@@ -25,7 +25,7 @@ router.put("/employee/:id",[
     check("phone").notEmpty().withMessage("Ingresa el número telefónico."),
     check("email").isEmail().withMessage("Ingresa el correo electrónico."),
     check("password").notEmpty().withMessage("Ingresa la contraseña."),
-    check("state").optional(),
+    check("state").optional().isBoolean(),
 ],UserController.userUpdateEmployee);
 
 export default router;
