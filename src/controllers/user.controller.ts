8560158a7df3dd6d80510f3d1b1805269344ab4e
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-03-11 01:12:57 -0500

    Update Backend

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -9,7 +9,7 @@ const saltRounds = 10;
 
 
 export const userRegisterEmploye = async (req: Request, res: Response) => {
-    
+
     const { shopping_id, first_name, last_name, phone, email, password } = req.body;
 
     const errors = validationResult(req);
@@ -34,10 +34,10 @@ export const userRegisterEmploye = async (req: Request, res: Response) => {
     }
 }
 
-export const userUpdateEmployee =  async (req: Request, res: Response) => {
+export const userUpdateEmployee = async (req: Request, res: Response) => {
     const user_exist = await pool.query(`SELECT * FROM "Employes" WHERE id = $1`, [req.params.id]);
     if (user_exist.rows.length <= 0) {
-      return res.status(400).json("El usuario no existe.");
+        return res.status(400).json("El usuario no existe.");
     }
 
     const errors = validationResult(req);
@@ -45,28 +45,28 @@ export const userUpdateEmployee =  async (req: Request, res: Response) => {
         return res.status(400).json({ errors: errors.array() });
     }
 
-    
-    const {shopping_id, kiosko_id, first_name, last_name, phone, email, password } = req.body;
-
-    try{
+    const { shopping_id, kiosko_id, first_name, last_name, phone, email, password } = req.body;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(password, salt);
+    try {
         const response = await pool.query(`
         UPDATE public."Employes"
         SET shopping_id=$2, kiosko_id=$3, first_name=$4, last_name=$5, phone=$6, email=$7, "password"=$8,  update_at=now()
         WHERE id=$1;
-        `, [req.params.id, shopping_id, kiosko_id, first_name, last_name, phone, email, password]);
+        `, [req.params.id, shopping_id, kiosko_id, first_name, last_name, phone, email, hash]);
         return res.status(200).json(response.rows);
-    }catch(e){
+    } catch (e) {
         loggsConfig.error(`${e}`);
         return res.status(500).json(e);
     }
 
 }
 
-export const userDeleteEmployee =  async (req: Request, res: Response) => {
+export const userDeleteEmployee = async (req: Request, res: Response) => {
 
     const user_exist = await pool.query(`SELECT * FROM "Employes" WHERE id = $1`, [req.params.id]);
     if (user_exist.rows.length <= 0) {
-      return res.status(400).json("El usuario no existe.");
+        return res.status(400).json("El usuario no existe.");
     }
 
     try {
