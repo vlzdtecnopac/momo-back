2ceb5ce5ad10c84caf6a683ba969ca44884b8cd8
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-03-13 12:57:31 -0500

    Update Backend

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -154,7 +154,7 @@ export const userDeleteEmployee = async (req: Request, res: Response) => {
 };
 
 export const userUpdateKioskoEmployee = async (req: Request, res: Response) => {
-    const user_exist = await pool.query("SELECT * FROM \"Employes\" WHERE id = $1", [req.params.id]);
+    const user_exist = await pool.query("SELECT * FROM \"Employes\" WHERE employee_id = $1", [req.params.id]);
     if (user_exist.rows.length <= 0) {
         return res.status(400).json("El usuario no existe.");
     }
@@ -170,7 +170,7 @@ export const userUpdateKioskoEmployee = async (req: Request, res: Response) => {
         const response = await pool.query(`
         UPDATE "Employes"
         SET kiosko_id=$2, update_at=now()
-        WHERE id=$1;
+        WHERE employee_id=$1;
         `, [req.params.id, kiosko_id]);
         return res.status(200).json(response.rows);
     } catch (e) {
