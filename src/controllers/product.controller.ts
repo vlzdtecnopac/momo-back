aa7400b26152cc8680526eef09e078ce6559482b
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-03-14 22:54:24 -0500

    Update Backend

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -3,7 +3,9 @@ import { v4 as uuidv4 } from "uuid";
 import { pool } from "../config/db";
 import { LoggsConfig } from "../config/logs";
 import { validationResult } from "express-validator";
-import Server from "../app";
+
+const loggsConfig: LoggsConfig = new LoggsConfig();
+
 
 export const createProduct = async (req: Request, res: Response) => {
     const errors = validationResult(req);
@@ -11,14 +13,19 @@ export const createProduct = async (req: Request, res: Response) => {
       return res.status(400).json({ errors: errors.array() });
     }
 
+    try{
     const { category_id, name_product, description, state} =  req.body;
     
     let Query = `INSERT INTO public."Product"
     (product_id, category_id, name_product, image, description, state, create_at)
-    VALUES('$1', '$2', '$3', '$4', '$5', false, now()) RETURNING category_id;
+    VALUES($1, $2, $3, $4, $5, $6, now()) RETURNING product_id;
     `;
 
     const response = await pool.query(Query, [uuidv4(), category_id, name_product, ,description, state]);
 
     return res.status(200).json(response.rows[0]);
+    }catch(e){
+      loggsConfig.error(`${e}`);
+      return res.status(500).json(e);
+    }
 };
