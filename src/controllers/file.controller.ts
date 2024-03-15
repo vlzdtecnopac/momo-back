d1b4d745bc9bcd5f87f0fff9f154cca9b34bbf33
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-03-14 19:13:01 -0500

    Upload S3

--- src/controllers/file.controller.ts
+++ src/controllers/file.controller.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from "express";
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { S3Client, PutObjectCommand, GetObjectCommand, ListObjectsCommand } from "@aws-sdk/client-s3";
+import {getSignedUrl} from '@aws-sdk/s3-request-presigner';
 import fs from "fs";
 
+if (process.env.NODE_ENV === "development") {
+    require("dotenv").config({ path: ".env.development" });
+  } else if (process.env.NODE_ENV === "production") {
+    require("dotenv").config({ path: ".env.production" });
+  }
+  
 const accessKeyId: any = process.env.AWS_PUBLIC_KEY;
 const secretAccessKey: any = process.env.AWS_SECRET_KEY;
 const region: any = process.env.AWS_BUCKET_REGION;
@@ -14,14 +21,13 @@ const s3Client = new S3Client({
     }
 });
 
+
 export const uploadFile = async (req: Request, res: Response) => {
     try {
         if (!req.files || !req.files.file) {
             return res.status(400).json({ error: "No se ha subido ningún archivo" });
         }
-
         const file = req.files.file;
-
         await uploadFileArchive(file);
         res.status(200).json("Archivo subido exitosamente a S3");
     } catch (err) {
@@ -29,6 +35,27 @@ export const uploadFile = async (req: Request, res: Response) => {
         res.status(500).json({ error: "Error interno del servidor" });
     }
 };
+
+export const getFile = async(req: Request, res: Response) => {
+    const result = await getFileGenerateURL(req.params.fileName);
+    return res.status(200).json({
+        url: result
+    });
+}
+
+export const getFiles =  async (req: Request, res: Response) => {
+    const result =  await getFilesURL();
+    return res.status(200).json(result.Contents);
+}
+
+async function getFilesURL(){
+    const command = new ListObjectsCommand({
+        Bucket: process.env.AWS_BUCKET_NAME
+    })
+    return await s3Client.send(command);
+}
+
+
 
 async function uploadFileArchive(file: any) {
     const fileContent = fs.createReadStream(file.tempFilePath);
@@ -41,9 +68,18 @@ async function uploadFileArchive(file: any) {
 
     try {
         const data: any = await s3Client.send(new PutObjectCommand(params));
-        console.log("Archivo cargado exitosamente en S3:", data.Location);
+        console.log("Archivo cargado exitosamente en S3:", data);
     } catch (err) {
         console.error("Error al subir el archivo a S3:", err);
         throw err; // Lanza el error para manejarlo en la función de llamada
     }
+}
+
+
+async function getFileGenerateURL(filename: any) {
+    const command = new GetObjectCommand({
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: filename
+    })
+    return await getSignedUrl(s3Client, command, { expiresIn: 3600 })
 }
--- src/routes/file.route.ts
+++ src/routes/file.route.ts
@@ -3,4 +3,6 @@ import {Router} from "express";
 const router: Router = Router();
 
 router.post("/", fileController.uploadFile);
+router.get("/:fileName", fileController.getFile);
+router.get("/", fileController.getFiles);
 export default router;
