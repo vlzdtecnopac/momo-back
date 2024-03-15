15eeb0991f534d5d6a7a565c3c63b75f9f203106
vlzdtecnopac <davidvalenzuela@tecnopac.com.co>
2024-03-15 00:52:06 -0500

    Update Backend

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -6,7 +6,7 @@ import { validationResult } from "express-validator";
 
 const loggsConfig: LoggsConfig = new LoggsConfig();
 
-export const updateCategory = async(req: Request, res: Response) => {
+export const updateCategory = async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
@@ -25,13 +25,24 @@ export const updateCategory = async(req: Request, res: Response) => {
     }
 }
 
-export const deleteCategory = async(req: Request, res: Response) => {
-
+export const deleteCategory = async (req: Request, res: Response) => {
     try {
+
+        let category_id = req.params.category_id;
+
+        const category_exist = await pool.query(`SELECT category_id
+        FROM "Category" WHERE category_id =  $1;
+   `, [category_id]);
+
+        if (category_exist.rows.length <= 0) {
+            return res.status(400).json("La categoria que desea eliminar no existe.");
+        }
+
+
         let Query = `DELETE FROM public."Category"
         WHERE category_id=$1 RETURNING category_id;
     `;
-        const response = await pool.query(Query, [req.params.category_id]);
+        const response = await pool.query(Query, [category_id]);
         return res.status(200).json(response.rows[0]);
     } catch (e) {
         loggsConfig.error(`${e}`);
--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -36,24 +36,33 @@ export const updateProduct = async (req: Request, res: Response) => {
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-try{
-  const { category_id, name_product, image, description, state } = req.body;
-  let Query = `UPDATE "Product"
+  try {
+    const { category_id, name_product, image, description, state } = req.body;
+    let Query = `UPDATE "Product"
   SET category_id=$1, name_product=$2, image=$3, description=$4, state=$5, update_at=now() WHERE product_id=$6 RETURNING product_id;`;
-  const response = await pool.query(Query, [category_id, name_product, image, description, state, req.params.product_id]);
-  return res.status(200).json(response.rows[0]);
-} catch (e) {
-  loggsConfig.error(`${e}`);
-  return res.status(500).json(e);
-}
+    const response = await pool.query(Query, [category_id, name_product, image, description, state, req.params.product_id]);
+    return res.status(200).json(response.rows[0]);
+  } catch (e) {
+    loggsConfig.error(`${e}`);
+    return res.status(500).json(e);
+  }
 
 }
 
 export const deleteProduct = async (req: Request, res: Response) => {
-  try{
+  let product_id = req.params.product_id;
+
+  const product_exist = await pool.query(`SELECT product_id FROM "Product" WHERE  product_id = $1
+`, [product_id]);
+
+  if (product_exist.rows.length <= 0) {
+    return res.status(400).json("El producto que desea eliminar no existe.");
+  }
+
+  try {
     let Query = `DELETE FROM "Product"
     WHERE product_id=$1 RETURNING product_id;`;
-    const response = await pool.query(Query, [req.params.product_id]);
+    const response = await pool.query(Query, [product_id]);
     return res.status(200).json(response.rows[0]);
   } catch (e) {
     loggsConfig.error(`${e}`);
